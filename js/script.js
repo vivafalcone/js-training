db465463317d09f517358367fa6ac43fe9723bfa
orbis1658 <orbis1658@gmail.com>
2020-04-02 22:47:07 +0900

    modify

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+//HTML要素を変数化
 let tanaka = document.getElementById("tanaka");
 
 let watanabe = "渡部";
 
 tanaka.textContent = watanabe;
+
+const hantei = () => {
+    let tanakaInner = tanaka.innerHTML;
+    let result = tanakaInner == "田中";
+    console.log(result);
+    
+}
+
+hantei();
